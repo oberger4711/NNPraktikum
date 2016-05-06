d7a4bf9463b753d2178f9e0f935835106e61770c
oberger <ole.k.berger@t-online.de>
2016-05-06 15:59:33 +0200

    Change author.

--- src/model/stupid_recognizer.py
+++ src/model/stupid_recognizer.py
@@ -3,8 +3,8 @@
 from random import random
 from model.classifier import Classifier
 
-__author__ = "ABC XYZ"  # Adjust this when you copy the file
-__email__ = "ABC.XYZ@student.kit.edu"  # Adjust this when you copy the file
+__author__ = "Ole Berger"  # Adjust this when you copy the file
+__email__ = "unexz@student.kit.edu"  # Adjust this when you copy the file
 
 
 class StupidRecognizer(Classifier):
