6e334aaa657110efae97b7c3d56a421a007c82bc
oberger <ole.k.berger@t-online.de>
2016-05-06 15:59:52 +0200

    Remove bias and add error factor.
    - Reach about 95% accuracy with enough epochs.

--- src/model/perceptron.py
+++ src/model/perceptron.py
@@ -8,6 +8,9 @@ import numpy as np
 from util.activation_functions import Activation
 from model.classifier import Classifier
 
+__author__ = "Ole Berger"  # Adjust this when you copy the file
+__email__ = "unexz@student.kit.edu"  # Adjust this when you copy the file
+
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.DEBUG,
                     stream=sys.stdout)
@@ -33,12 +36,11 @@ class Perceptron(Classifier):
     validationSet : list
     testSet : list
     weight : list
-    bias : float
     """
-    def __init__(self, train, valid, test, learningRate=0.01, epochs=50):
+    def __init__(self, train, valid, test, learningRate=0.1, epochs=50):
 
         self.learningRate = learningRate
-        self.epochs = epochs
+        self.epochs = min(epochs, len(train.input))
 
         self.trainingSet = train
         self.validationSet = valid
@@ -46,8 +48,7 @@ class Perceptron(Classifier):
 
         # Initialize the weight vector with small random values
         # around 0 and 0.1
-        self.weight = np.random.rand(self.trainingSet.input.shape[1])/10
-        self.bias = -100.
+        self.weight = np.random.rand(self.trainingSet.input.shape[1] + 1)/10
 
     def train(self, verbose=True):
         """Train the perceptron with the perceptron learning algorithm.
@@ -59,13 +60,21 @@ class Perceptron(Classifier):
         """
 	# Here you have to implement the Perceptron Learning Algorithm
         # to change the weights of the Perceptron
-        for label, data in zip(self.trainingSet.label, self.trainingSet.input):
-            if self.classify(data) != label:
-                self.weight = np.asarray(np.array(self.weight) + self.learningRate * np.array(data))
-                if verbose:
-                    print "Adjusted weights after wrong classification."
-            elif verbose:
-                    print "Correct classification."
+        for i in range(0, self.epochs):
+            label, data = self.trainingSet.label[i], self.trainingSet.input[i]
+            error = label - self.classify(data)
+            weightStep = self.learningRate * error
+            self.weight[1:] += weightStep * np.array(data)
+            self.weight[0] += weightStep
+            if verbose:
+                logging.info("New validation accuracy after adjusting weights: %f", self.validateAccuracy())
+    
+    def validateAccuracy(self):
+        correctClassifications = 0
+        for label, data in zip(self.validationSet.label, self.validationSet.input):
+            if self.classify(data) == label:
+                correctClassifications += 1
+        return float(correctClassifications) / len(self.validationSet.input)
 
     def classify(self, testInstance):
         """Classify a single instance.
@@ -79,7 +88,6 @@ class Perceptron(Classifier):
         bool :
             True if the testInstance is recognized as a 7, False otherwise.
         """
-        # Hard limiter.
         return self.fire(testInstance)
 
     def evaluate(self, test=None):
@@ -107,4 +115,4 @@ class Perceptron(Classifier):
     def fire(self, input):
         """Fire the output of the perceptron corresponding to the input """
         # I already implemented it for you to see how you can work with numpy
-        return Activation.sign(np.dot(np.array(input), self.weight) + self.bias)
+        return Activation.sign(np.dot(np.array(input), self.weight[1:]) + self.weight[0])
