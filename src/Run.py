f4d11a558512800a30ab3d954d7595ddf8b9fbfa
oberger <ole.k.berger@t-online.de>
2016-05-09 17:09:21 +0200

    Change epochs to 50 as it seems to give slightly better results.

--- src/Run.py
+++ src/Run.py
@@ -18,7 +18,7 @@ def main():
                                           data.validationSet,
                                           data.testSet,
                                           learningRate=0.005,
-                                          epochs=30)
+                                          epochs=50)
 
     # Train the classifiers
     print("=========================")
