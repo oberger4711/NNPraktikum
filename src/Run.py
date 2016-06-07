fe6c1b8f069f143a8c2a69f46df9eb7f8a83173d
oberger <ole.k.berger@t-online.de>
2016-06-07 21:13:17 +0200

    Separate classifier evaluation into multiple functions.
    - Functions are very redundant yet.

--- src/Run.py
+++ src/Run.py
@@ -9,70 +9,88 @@ from report.evaluator import Evaluator
 
 
 def main():
+    #runStupidClassifier()
+    #runPerceptronClassifier()
+    runLogisticClassifier()
+
+
+def runStupidClassifier():
     data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000)
-    myStupidClassifier = StupidRecognizer(data.trainingSet,
-                                          data.validationSet,
-                                          data.testSet)
-    myPerceptronClassifier = Perceptron(data.trainingSet,
-                                        data.validationSet,
-                                        data.testSet,
-                                        learningRate=0.005,
-                                        epochs=10)
-    # Uncomment this to run Logistic Neuron Layer
-    myLRClassifier = LogisticRegression(data.trainingSet,
-                                        data.validationSet,
-                                        data.testSet,
-                                        learningRate=0.005,
-                                        epochs=30)
-
-    # Train the classifiers
-    print("=========================")
-    print("Training..")
-    print("")
-
+    myStupidClassifier = StupidRecognizer(data.training_set,
+                                          data.validation_set,
+                                          data.test_set)
+    # Train
     print("Train Stupid Classifier..")
     myStupidClassifier.train()
     print("Done..")
     print("")
 
-    print("Train Perceptron..")
-    # Change parameter to True for verbose output.
-    #myPerceptronClassifier.train(True)
+    # Evaluate
+    print("Evaluate..")
+    stupidPred = myStupidClassifier.evaluate()
     print("Done..")
     print("")
 
-    print("Train Logistic Regression..")
+    # Results
+    evaluator = Evaluator()
+    print("Result of the stupid recognizer:")
+    # evaluator.printComparison(data.test_set, stupidPred)
+    evaluator.printAccuracy(data.test_set, stupidPred)
+    print("")
+
+def runPerceptronClassifier():
+    data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000)
+    myPerceptronClassifier = Perceptron(data.training_set,
+                                        data.validation_set,
+                                        data.test_set,
+                                        learningRate=0.005,
+                                        epochs=10)
+
+    # Train
+    print("Train Perceptron Classifier..")
+    myPerceptronClassifier.train(True)
+    print("Done..")
+    print("")
+
+    # Evaluate
+    print("Evaluate..")
+    perceptronPred = myPerceptronClassifier.evaluate()
+    print("Done..")
+    print("")
+
+    # Results
+    evaluator = Evaluator()
+    print("Result of the Perceptron recognizer:")
+    # evaluator.printComparison(data.test_set, perceptronPred)
+    evaluator.printAccuracy(data.test_set, perceptronPred)
+    print("")
+
+def runLogisticClassifier():
+    data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000)
+    myLRClassifier = LogisticRegression(data.training_set,
+                                        data.validation_set,
+                                        data.test_set,
+                                        learningRate=0.005,
+                                        epochs=30)
+
+    # Train
+    print("Train Logistic Classifier..")
     myLRClassifier.train(graph=True)
     print("Done..")
     print("")
-
-    # Do the recognizer
-    # Explicitly specify the test set to be evaluated
-    stupidPred = myStupidClassifier.evaluate()
-    #perceptronPred = myPerceptronClassifier.evaluate()
+    
+    # Evaluate
+    print("Evaluate..")
     lrPred = myLRClassifier.evaluate()
-
-    # Report the result
-    print("=========================")
-    print("Results..")
+    print("Done..")
     print("")
+
+    # Results
     evaluator = Evaluator()
-
-    print("Result of the stupid recognizer:")
-    # evaluator.printComparison(data.testSet, stupidPred)
-    evaluator.printAccuracy(data.testSet, stupidPred)
-    print("")
-
-    print("Result of the Perceptron recognizer:")
-    # evaluator.printComparison(data.testSet, perceptronPred)
-    #evaluator.printAccuracy(data.testSet, perceptronPred)
-    print("")
-
     print("Result of the Logistic Regression recognizer:")
-    # evaluator.printComparison(data.testSet, perceptronPred)
-    evaluator.printAccuracy(data.testSet, lrPred)
+    # evaluator.printComparison(data.test_set, perceptronPred)
+    evaluator.printAccuracy(data.test_set, lrPred)
     print("")
-
 
 if __name__ == '__main__':
     main()
