4fe9d23eafe8c61e3f2adaf076f367c1eec6b097
oberger <ole.k.berger@t-online.de>
2016-06-22 13:45:40 +0200

    Add 100 neurons hidden layer and increse learning_rate.

--- src/Run.py
+++ src/Run.py
@@ -68,9 +68,9 @@ def runMultilayerClassifier(data):
     c = MultilayerPerceptron(data.training_set,
                             data.validation_set,
                             data.test_set,
-                            learning_rate=0.01,
+                            learning_rate=0.05,
                             epochs=20,
-                            n_neurons_per_layer=[784, 10])
+                            n_neurons_per_layer=[784, 100, 10])
     trainAndEvaluateClassifier(c, data.test_set, verbose=True, graph=True)
 
 if __name__ == '__main__':
